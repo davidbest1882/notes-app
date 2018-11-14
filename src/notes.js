30c6fa6f5346074b7462e0c48f91d1dd1d86edbb
Sundar Venkataraman <phi.pax@gmail.com>
2018-11-14 13:16:09 +0000

    Included code to display the notes array and used CSS/JS to hide and display content usng accordion

--- src/notes.js
+++ src/notes.js
@@ -1,27 +1,70 @@
 
-var notes = function Note() {
-  // {"This is a full note w": "This is a full note with lots of characters and it is long and stuff.",
-  //   "chicken dippers are ": "chicken dippers are really fun in the sun and are naked sometimes",
-  //   "cheese twirls make t": "cheese twirls make the world a fun place chicken"}
+// Code below till line 11 commented by SVR
+//var notes = function Note() {
+//   // {"This is a full note w": "This is a full note with lots of characters and it is long and stuff.",
+//   //   "chicken dippers are ": "chicken dippers are really fun in the sun and are naked sometimes",
+//   //   "cheese twirls make t": "cheese twirls make the world a fun place chicken"}
+//
+//   Note.prototype.allNotes = function() {
+//     return this._allNotes
+//   }
+// }
 
-  Note.prototype.allNotes = function() {
-    return this._allNotes
-  }
-}
-
+// Module below was created by Dave to addNote to a Notes array
 (function(anyObj){
   var notes = [];
-  
+
   function addNote(){
     var note = document.getElementById("name").value;
     if (note === "") {
       document.getElementById("stat_msg").innerHTML = "No message entered";
     } else {
       notes.push(note);
+      // Line below included by SVR - Known issue contents replicated div needs to be reloaded
+      displayNotes();
     };
   }
+
+// method added by SVR below for testing accordion
+  function displayNotes(){
+      var di = document.getElementById("display-notes");
+
+      for(var i=0;i<notes.length;i++){
+        var nodeB = document.createElement("BUTTON");
+        nodeB.setAttribute("class","accordion");
+        var abbTxt = notes[i].slice(0,20)
+        var textnodeB = document.createTextNode(abbTxt);
+        nodeB.appendChild(textnodeB);
+        di.appendChild(nodeB);
+        var nodeD = document.createElement("DIV");
+        nodeD.setAttribute("class","panel");
+        var nodeP = document.createElement("P")
+        var textnodeP = document.createTextNode(notes[i]);
+        nodeP.appendChild(textnodeP);
+        nodeD.appendChild(nodeP);
+        di.appendChild(nodeD);
+      };
+
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
+  }
+// SVR method ends *******************************
+
   anyObj.addNote = addNote;
   anyObj.notes = notes;
+  anyObj.displayNotes = displayNotes;
 })(this)
 
+// Dave's Module ends here ******************************************
+//document.getElementsByTagName("body").onload = function(){displayNotes()};
 document.getElementById("post_it").onclick = function(){addNote()};
